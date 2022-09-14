9f2a9e49e782229798ffd2d920a31fe24ef022bc
Yogi Satya <yogisatyamahayana@gmail.com>
2022-09-14 13:08:20 +0800

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -4,4 +4,44 @@ const data_projects = [
     "title": "<h3>Sistem Informasi Sekolah</h3>",
     "content": '<div>Manajemen data SPP, Keuangan dan Siswa</div><a href="https://sis.pratiwischool.com">Link Project</a>'
   },
+  {
+    "data_date": "2022-01",
+    "title": "<h3>Portal Informasi & Web Lomba GLM Undiksha 2021 - 2022</h3>",
+    "content": '<div>Portal informasi dan web admin & peserta lomba GLM Undiksha dari tahun 2021 - 2022</div><a href="https://glmundiksha.com/">Link Project</a>'
+  },
+  {
+    "data_date": "2021-05",
+    "title": "<h3>POS (Point Of Sale) Toko Panji Decorindo</h3>",
+    "content": '<div>Aplikasi kasir</div><a href="#">Offline Project</a>'
+  },
+  {
+    "data_date": "2021-02",
+    "title": "<h3>Sistem HRD & Penggajian Karyawan</h3>",
+    "content": '<div>Website manajemen data HRD & Karyawan</div><a href="https://eastern-kayan.com/hrd">Link Project</a>'
+  },
+  {
+    "data_date": "2021-01",
+    "title": "<h3>Sistem Pengajuan Surat Kantor Imigrasi Singaraja</h3>",
+    "content": '<div>Website pengajuan surat</div><a href="https://gitlab.com/giksworld-project/project-simigrasi-2.0">Private Project</a>'
+  },
+  {
+    "data_date": "2021-01",
+    "title": "<h3>Website Pemira Jurusan Manajemen Undiksha 2021</h3>",
+    "content": '<div>Website pemilihan ketua HMJ manajemen Undiksha</div><a href="https://pemira.manajemen-undiksha.com/">Private Project</a>'
+  },
+  {
+    "data_date": "2020-10",
+    "title": "<h3>Website Perijinan & Mobile API DISKOMINFO GIANYAR</h3>",
+    "content": '<div>Prakerin</div><a href="https://prakerin.giksworld.my.id/web-perijinan">Link Project</a>'
+  },
+  {
+    "data_date": "2020-01",
+    "title": "<h3>Website E-Vote Ketua HMJ PTI 2020</h3>",
+    "content": '<div>Website pemilihan ketua HMJ PTI 2020</div><a href="https://if.undiksha.ac.id/e-vote">Link Project</a>'
+  },
+  {
+    "data_date": "2019-06",
+    "title": "<h3>Website Toko Obat</h3>",
+    "content": '<div>Tugas UAS pemrograman web</div><a href="https://github.com/yogisatya2705/Uas-Web-Toko-Obat">Link Project</a>'
+  },
 ]
